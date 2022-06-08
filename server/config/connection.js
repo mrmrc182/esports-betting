e8269562f5a1dedfa5ffb834c9454afb90fb5159
Matthew Carlson <carlson.matt.r@gmail.com>
2022-06-08 10:52:32 -0700

    setting up heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/openlobbyDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
