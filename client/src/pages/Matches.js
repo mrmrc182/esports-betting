299673772259e72e38aae68e7f348743060db49f
Sam Wallace <sam@sammymayhem.com>
2022-06-09 10:26:51 -0700

    added color style to match page and css

--- client/src/pages/Matches.js
+++ client/src/pages/Matches.js
@@ -4,28 +4,40 @@ import '../styles/Matches.css'
 
 export default function Matches() {
     return (
-        <div id="match-cards">
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+        <div className='match-cont'>
+            <div id='parent'>
+                <h1>Match-ups</h1>
 
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+                <div id="match-cards">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={process.env.PUBLIC_URL + 'assets/images/faze.jpeg'} />
+                        <Card.Body>
+                            <Card.Title>Team A</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+
+                    <div>
+                        <h2>
+                            VS
+                        </h2>
+                    </div>
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={process.env.PUBLIC_URL + 'assets/images/cloud9.jpeg'} />
+                        <Card.Body>
+                            <Card.Title>Team B</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
         </div>
     )
 }
