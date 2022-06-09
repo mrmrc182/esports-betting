c52184f7410676ec5308340cf685bc9aa9fdda2b
Matthew Carlson <carlson.matt.r@gmail.com>
2022-06-08 18:13:49 -0700

    added basic styles to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useAuth } from "../util/auth";
 import {Card} from "react-bootstrap";
+import "../styles/Home.css";
 
 
 export default function Home() {
@@ -9,15 +10,15 @@ export default function Home() {
       <h1>Welcome {isLoggedIn ? user.username : "Guest"}!</h1>
       <hr />
       <br></br>
-      <Card style={{textAlign: "left", border: "5px solid black", backgroundColor: "lightgray"}}>
+      <Card class="homepage-cards">
         <Card.Body><h3>Play for FREE!</h3></Card.Body>
       </Card>
       <br></br>
-      <Card style={{textAlign: "right", border: "5px solid black", backgroundColor: "lightgray"}}>
+      <Card class="homepage-cards">
         <Card.Body><h3>Earn Credits Weekly</h3></Card.Body>
       </Card>
       <br></br>
-      <Card style={{textAlign: "left", border: "5px solid black", backgroundColor: "lightgray"}}>
+      <Card class="homepage-cards">
         <Card.Body><h3>Place bets on CS:GO, LoL, Overwatch, Dota 2, and many more!</h3></Card.Body>
       </Card>
       <br></br><br></br>
