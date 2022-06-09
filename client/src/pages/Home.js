dd69437aae16a46f0261da303f4d965fd4ddb5de
Matthew Carlson <carlson.matt.r@gmail.com>
2022-06-09 10:58:02 -0700

    updated fonts and cleaned some stuff up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,19 +7,19 @@ export default function Home() {
   const { isLoggedIn, user } = useAuth();
   return (
     <div>
-      <h1>Welcome {isLoggedIn ? user.username : "Guest"}!</h1>
+      <h1 style={{textAlign: "center"}}>Welcome {isLoggedIn ? user.username : "Guest"} to Open Lobby!</h1>
       <hr />
       <br></br>
-      <Card class="homepage-cards">
-        <Card.Body><h3>Play for FREE!</h3></Card.Body>
+      <Card className="homepage-cards">
+        <Card.Body className="background"><h3>Play for FREE!</h3></Card.Body>
       </Card>
       <br></br>
-      <Card class="homepage-cards">
-        <Card.Body><h3>Earn Credits Weekly</h3></Card.Body>
+      <Card className="homepage-cards-right">
+        <Card.Body className="background"><h3>Earn Credits Weekly</h3></Card.Body>
       </Card>
       <br></br>
-      <Card class="homepage-cards">
-        <Card.Body><h3>Place bets on CS:GO, LoL, Overwatch, Dota 2, and many more!</h3></Card.Body>
+      <Card className="homepage-cards">
+        <Card.Body className="background"><h3>Place bets on CS:GO, LoL, Overwatch, Dota 2, and many more!</h3></Card.Body>
       </Card>
       <br></br><br></br>
     </div>
