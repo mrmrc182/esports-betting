b1ed85dd06c8c41f5632dbbd1763be5983dafca0
Sam Wallace <sam@sammymayhem.com>
2022-06-12 13:52:38 -0700

    finished style for login/signup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
 import { useLocation, Navigate } from "react-router-dom";
 import { useAuth } from "../util/auth";
+import { Form, Row, Col, Button } from 'react-bootstrap';
+import '../styles/Login.css';
 
 // This signup form is intentionally minimalist to reduce effort required to
 // customize it to your app's needs. See the excellent best practices guide for
 // sign informs on web.dev https://web.dev/sign-in-form-best-practices/
 
 // TODO: customize styles or import styles with favorite css approach
-const styles = {
-  formControl: {
-    display: "flex",
-    padding: "0.25em",
-  },
-  label: {
-    flex: "0 1 6em",
-    paddingRight: "0.25em",
-  },
-};
+// const styles = {
+//   formControl: {
+//     display: "flex",
+//     padding: "0.25em",
+//   },
+//   label: {
+//     flex: "0 1 6em",
+//     paddingRight: "0.25em",
+//   },
+// };
 
 const initialFormState = {
   email: "",
@@ -52,43 +54,47 @@ export default function Login() {
   }
 
   return (
-    <div>
-      <h1 className="center" id="bottom">Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div style={styles.formControl} className="center">
-          <label htmlFor="email" style={styles.label} className="label">
-            Email
-          </label>
-          <input
-            disabled={loading}
-            id="email"
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            value={formState.email.value}
-            onChange={handleInputChange}
-          />
+    <div className="login-cont">
+      <div className="login-box">
+
+        <div className="form-cont">
+          <h1 className="login-title">Login</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+              <Form.Label className="form-label">
+                Email
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control
+                  disabled={loading}
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Enter email"
+                  value={formState.email.value}
+                  onChange={handleInputChange} />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+              <Form.Label className="form-label">
+                Password
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control
+                  disabled={loading}
+                  id="new-password"
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                  value={formState.password.value}
+                  onChange={handleInputChange} />
+              </Col>
+            </Form.Group>
+            <Button className="login-btn" variant="outline-primary" disabled={loading} type="submit">{loading ? "Loading..." : "Login"}</Button>
+          </Form>
         </div>
-        <div style={styles.formControl} className="center">
-          <label htmlFor="new-password" style={styles.label} className="label">
-            Password
-          </label>
-          <input
-            disabled={loading}
-            id="new-password"
-            type="password"
-            name="password"
-            placeholder="Enter password"
-            value={formState.password.value}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div style={styles.formControl} className="center">
-          <button disabled={loading} type="submit">
-            {loading ? "Loading..." : "Submit"}
-          </button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
