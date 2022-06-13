581f14bd6da759cddc75994d99879bbf9548019c
JK <jrileykruger@gmail.com>
2022-06-12 23:20:59 -0700

    added dynamic betaccordion to profile page

---
+++ client/src/components/BetAccordion.js
@@ -0,0 +1,13 @@
+import { Accordion } from "react-bootstrap";
+import "../styles/Profile.css";
+
+export default function BetAccordion(props) {
+
+  return (
+    <Accordion.Item eventKey={props.eventKey} className="prof-cards">
+    <Accordion.Header>{props.teamA} vs {props.teamB}</Accordion.Header>
+    <Accordion.Body>Picked to Win: {props.choiceName}</Accordion.Body>
+    <Accordion.Body>Bet Amount: {props.amount}</Accordion.Body>
+    </Accordion.Item>
+  );
+}
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,6 +3,7 @@ import { Container, Accordion } from "react-bootstrap";
 import "../styles/Profile.css";
 import { BETS } from "../util/queries";
 import { useQuery } from "@apollo/client";
+import BetAccordion from "../components/BetAccordion";
 
 export default function Profile() {
 
@@ -13,49 +14,53 @@ export default function Profile() {
 
   return (
     <div className="center">
+      {loading ? (<div>loading...</div>) : (
+        <div>
+        <div>
+          <h4 className="credit-score">Credits: 1000</h4>
+        </div>
+  
+        <br></br>
+        <h1>Open Bets</h1>
 
-      <div>
-        <h4 className="credit-score">Credits: 1000</h4>
-      </div>
+        <div className="open-cards">
+          <Container className="container-width">
+            <Accordion>
+              {data.bets.slice(0).reverse().map((bet, index) => (
+                <BetAccordion 
+                  key={index}
+                  eventKey={index}
+                  teamA={bet.teamA}
+                  teamB={bet.teamB}
+                  choiceName={bet.choiceName}
+                  amount={bet.amount}
+                />
+              ))}
+            </Accordion>
+          </Container>
+        </div>
+  
+        <div className="closed-cards">
+          <Container className="container-width">
+            <br></br>
+            <h1>Closed Bets</h1>
+            <Accordion>
+              <Accordion.Item eventKey="0" className="prof-cards">
+                <Accordion.Header>FaZe vs Cloud9</Accordion.Header>
+                <Accordion.Body>Winner: FaZe</Accordion.Body>
+                <Accordion.Body>Bet Amount: 400</Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1" className="prof-cards">
+                <Accordion.Header>FaZe vs South Korea</Accordion.Header>
+                <Accordion.Body>Winner: South Korea</Accordion.Body>
+                <Accordion.Body>Bet Amount: 400</Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Container>
+        </div>
+        </div>
+      )}
 
-      <br></br>
-      <h1>Open Bets</h1>
-      <div className="open-cards">
-        <Container className="container-width">
-          <Accordion>
-            <Accordion.Item eventKey="0" className="center prof-cards">
-              <Accordion.Header className="header">
-                FaZe vs Cloud9
-              </Accordion.Header>
-              <Accordion.Body>Picked to Win: FaZe</Accordion.Body>
-              <Accordion.Body>Bet Amount: 400</Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1" className="prof-cards">
-              <Accordion.Header>FaZe vs South Korea</Accordion.Header>
-              <Accordion.Body>Picked to Win: South Korea</Accordion.Body>
-              <Accordion.Body>Bet Amount: 400</Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </Container>
-      </div>
-      <div className="closed-cards">
-        <Container className="container-width">
-          <br></br>
-          <h1>Closed Bets</h1>
-          <Accordion>
-            <Accordion.Item eventKey="0" className="prof-cards">
-              <Accordion.Header>FaZe vs Cloud9</Accordion.Header>
-              <Accordion.Body>Winner: FaZe</Accordion.Body>
-              <Accordion.Body>Bet Amount: 400</Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1" className="prof-cards">
-              <Accordion.Header>FaZe vs South Korea</Accordion.Header>
-              <Accordion.Body>Winner: South Korea</Accordion.Body>
-              <Accordion.Body>Bet Amount: 400</Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </Container>
-      </div>
-    </div >
+    </div>
   );
 }
