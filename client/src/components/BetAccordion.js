5a94d8be0c2679b09582a076d69a65b998529e2a
Sam Wallace <sam@sammymayhem.com>
2022-06-23 13:56:36 -0700

    added to the look of the profile page

--- client/src/components/BetAccordion.js
+++ client/src/components/BetAccordion.js
@@ -5,9 +5,9 @@ export default function BetAccordion(props) {
 
   return (
     <Accordion.Item eventKey={props.eventKey} className="prof-cards">
-    <Accordion.Header>{props.teamA} vs {props.teamB}</Accordion.Header>
-    <Accordion.Body>Picked to Win: {props.choiceName}</Accordion.Body>
-    <Accordion.Body>Bet Amount: {props.amount}</Accordion.Body>
+      <Accordion.Header>{props.teamA} vs {props.teamB}</Accordion.Header>
+      <Accordion.Body>Picked to Win: {props.choiceName} | Bet Amount: {props.amount}</Accordion.Body>
+      {/* <Accordion.Body>Bet Amount: {props.amount}</Accordion.Body> */}
     </Accordion.Item>
   );
 }
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Accordion } from "react-bootstrap";
+import { Container, Accordion, Image } from "react-bootstrap";
 import "../styles/Profile.css";
 import { BETS } from "../util/queries";
 import { useQuery } from "@apollo/client";
@@ -7,7 +7,7 @@ import BetAccordion from "../components/BetAccordion";
 
 export default function Profile() {
 
-  const {loading, data} = useQuery(BETS, {
+  const { loading, data } = useQuery(BETS, {
     fetchPolicy: "no-cache",
   });
   console.log(loading, data);
@@ -15,49 +15,63 @@ export default function Profile() {
   return (
     <div className="center">
       {loading ? (<div>loading...</div>) : (
-        <div>
-        <div>
-          <h4 className="credit-score">Credits: 1000</h4>
-        </div>
-  
-        <br></br>
-        <h1>Open Bets</h1>
+        <div className="prof-container">
 
-        <div className="open-cards">
-          <Container className="container-width">
-            <Accordion>
-              {data.bets.slice(0).reverse().map((bet, index) => (
-                <BetAccordion 
-                  key={index}
-                  eventKey={index}
-                  teamA={bet.teamA}
-                  teamB={bet.teamB}
-                  choiceName={bet.choiceName}
-                  amount={bet.amount}
-                />
-              ))}
-            </Accordion>
-          </Container>
-        </div>
-  
-        <div className="closed-cards">
-          <Container className="container-width">
-            <br></br>
-            <h1>Closed Bets</h1>
-            <Accordion>
-              <Accordion.Item eventKey="0" className="prof-cards">
-                <Accordion.Header>FaZe vs Cloud9</Accordion.Header>
-                <Accordion.Body>Winner: FaZe</Accordion.Body>
-                <Accordion.Body>Bet Amount: 400</Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="1" className="prof-cards">
-                <Accordion.Header>FaZe vs South Korea</Accordion.Header>
-                <Accordion.Body>Winner: South Korea</Accordion.Body>
-                <Accordion.Body>Bet Amount: 400</Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
-          </Container>
-        </div>
+          {/* Side bar for profile info */}
+          <div className="side-bar">
+            <h3>username</h3>
+            <div>
+              <Image aria-label="user avatar"></Image>
+            </div>
+            <div>
+              <h4 className="credit-score">Credits: 1000</h4>
+            </div>
+          </div>
+
+          {/* Body container for profile bets */}
+          <div className="prof-body">
+
+            <div className="body-el">
+              <div className="prof-box">
+                <h1 className="open-title">Open Bets</h1>
+
+                <div className="open-cards">
+                  <Container className="container-width">
+                    <Accordion>
+                      {data.bets.slice(0).reverse().map((bet, index) => (
+                        <BetAccordion
+                          key={index}
+                          eventKey={index}
+                          teamA={bet.teamA}
+                          teamB={bet.teamB}
+                          choiceName={bet.choiceName}
+                          amount={bet.amount}
+                        />
+                      ))}
+                    </Accordion>
+                  </Container>
+                </div>
+
+                <div className="closed-cards">
+                  <Container className="container-width">
+                    <h1 className="closed-title">Closed Bets</h1>
+                    <Accordion>
+                      <Accordion.Item eventKey="0" className="prof-cards">
+                        <Accordion.Header>FaZe vs Cloud9</Accordion.Header>
+                        <Accordion.Body>Winner: FaZe</Accordion.Body>
+                        <Accordion.Body>Bet Amount: 400</Accordion.Body>
+                      </Accordion.Item>
+                      <Accordion.Item eventKey="1" className="prof-cards">
+                        <Accordion.Header>FaZe vs South Korea</Accordion.Header>
+                        <Accordion.Body>Winner: South Korea</Accordion.Body>
+                        <Accordion.Body>Bet Amount: 400</Accordion.Body>
+                      </Accordion.Item>
+                    </Accordion>
+                  </Container>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       )}
 
